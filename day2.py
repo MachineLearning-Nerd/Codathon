d00d4a672369dbafd8cb2b2f6d1b2bb43483b4a9
Dinesh <jinjaladinesh@gmail.com>
2020-01-14 22:39:06 +0530

    faster way

--- day2.py
+++ day2.py
@@ -1,18 +1,36 @@
+import pdb
+from sys import stdin, stdout
+import numpy as np
 N = int(input())
-A = []
+A = np.array([], dtype=int)
 for row in range(N):
-    rowval = list(map(int, input().split(' ')))
-    A.append(rowval)
+    rowval = stdin.readline()
+    rowval = np.fromstring(rowval, dtype=int, sep=' ')
+    # rowval = list(map(int, stdin.readline().strip().split(' ')))
+    # A.append(rowval[row])
+    # import pdb
+    # pdb.set_trace()
+    A = np.append(A, rowval[row])
+
 Qlen = int(input())
+prerow = np.array(A)
 for row in range(Qlen):
-    Qrow = list(map(int, input().split(' ')))
-    a = Qrow[1]
-    b = Qrow[2]
-    c = Qrow[3]
-    for i in range(len(A)):
-        if i in range(a-1, b):
-            A[i][i] = A[i][i] ^ c
-sumval = 0
-for i in range(len(A)):
-    sumval += A[i][i]
-print(sumval)
+    # Qrow = stdin.readline().split()
+    Qrow = stdin.readline()
+    Qrow = np.fromstring(Qrow, dtype=int, sep=' ')
+    # print(Qrow)
+    a = int(Qrow[1])
+    b = int(Qrow[2])
+    c = int(Qrow[3])
+    array1 = np.ones(b-a+1, dtype=int)*c
+    # array2 = np.zeros(a-1, dtype=int)
+    # array3 = np.zeros(N-b, dtype=int)
+    # newrow = np.append(np.append(array2, array1), array3)
+    # import pdb
+    # pdb.set_trace()
+    # newrow = np.append(np.zeros(a-1, dtype=int),
+    #                    np.ones(b-a+1, dtype=int)*c, np.zeros(N-b, dtype=int))
+    # import pdb
+    # pdb.set_trace()
+    prerow[a-1:b] = prerow[a-1:b] ^ array1
+print(prerow.sum())
