0990616ed13ee90518ec666fca563031f67fc9b8
Dinesh <jinjaladinesh@gmail.com>
2020-01-15 00:20:19 +0530

    final without numpy

--- day2.py
+++ day2.py
@@ -1,38 +1,30 @@
 from sys import stdin, stdout
-import numpy as np
 N = int(input())
-A = np.array([], dtype=int)
+A = []
 for row in range(N):
-    rowval = stdin.readline()
-    rowval = np.fromstring(rowval, dtype=int, sep=' ')
-    A = np.append(A, rowval[row])
+    rowval = stdin.readline().strip().split(' ')
+    A.append(int(rowval[row]))
 
 
 def split_int(listval):
     return list(map(int, listval.split(' ')))
 
 
-def Qfunction(N, A):
+def Qfunction(A):
     Qlen = int(input())
-    prerow = np.array(A)
-    Qrow = stdin.readlines()
-    # Qrow1 = list(map(split_int, Qrow))
-    # del Qrow
+    prerow = A
+    # Qrow = stdin.readlines()
+    # Qrow = list(map(split_int, Qrow))
 
-    i = 0
-    # while(i < Qlen):
-    for line in Qrow:
-        # line = Qrow[0]
-        # line = split_int(line)
-        line = line.strip().split(' ')
-        # Qrow.pop(0)
+    for line in stdin.readlines():
+        line = line.strip().split()
         a = int(line[1])
         b = int(line[2])
         c = int(line[3])
-        array = np.ones(b-a+1, dtype=int)*c
-        prerow[a-1:b] = np.bitwise_xor(prerow[a-1:b], array)
-        i += 1
-    print(prerow.sum())
+        array = [c]*(b-a+1)
+        for i, array_val in enumerate(array):
+            prerow[a-1+i] = prerow[a-1+i] ^ array_val
+    print(sum(prerow))
 
 
-Qfunction(N, A)
+Qfunction(A)
