7aea6bcef49d839d879cc51d04b69dd56b52c18e
Dinesh <jinjaladinesh@gmail.com>
2020-01-14 23:36:01 +0530

    day2 withour numpy

--- day2.py
+++ day2.py
@@ -1,29 +1,35 @@
-import pdb
 from sys import stdin, stdout
-import numpy as np
+# import numpy as np
 N = int(input())
-A = np.array([], dtype=int)
+# A = np.array([], dtype=int)
+A = []
 for row in range(N):
-    rowval = stdin.readline()
-    rowval = np.fromstring(rowval, dtype=int, sep=' ')
-    A = np.append(A, rowval[row])
+    rowval = stdin.readline().strip().split(' ')
+    # rowval = np.fromstring(rowval, dtype=int, sep=' ')
+    # A = np.append(A, rowval[row])
+    A.append(int(rowval[row]))
+print(A)
 
 
-def Qfunction(N, A):
-    Qlen = int(input())
-    for row in range(Qlen):
-        Qrow = stdin.readline()
-        Qrow = np.fromstring(Qrow, dtype=int, sep=' ')
-        a = int(Qrow[1])
-        b = int(Qrow[2])
-        c = int(Qrow[3])
-        array1 = np.ones(b-a+1, dtype=int)*c
-        yield a, b, array1
+def split_int(listval):
+    return list(map(int, listval.split(' ')))
 
 
-prerow = np.array(A)
-for a, b, array in Qfunction(N, A):
-    prerow[a-1:b] = np.bitwise_xor(prerow[a-1:b], array)
+Qlen = int(input())
+# prerow = np.array(A)
+prerow = A
+Qrow = stdin.readlines()
+Qrow = list(map(split_int, Qrow))
 
-
-print(prerow.sum())
+for line in Qrow:
+    a = line[1]
+    b = line[2]
+    c = line[3]
+    array = [c]*(b-a+1)
+    for i, array_val in enumerate(array):
+        prerow[a-1+i] = prerow[a-1+i] ^ array_val
+    # prerow[a-1:b] = prerow[a-1:b] ^ array
+    # array = np.ones(b-a+1, dtype=int)*c
+    # prerow[a-1:b] = np.bitwise_xor(prerow[a-1:b], array)
+# print(prerow.sum())
+print(sum(prerow))
