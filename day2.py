07064529224d76b1a8dba5ed54b3e5e27a1e92cf
Dinesh <jinjaladinesh@gmail.com>
2020-01-14 23:45:02 +0530

    with pop in Qfunction

--- day2.py
+++ day2.py
@@ -1,35 +1,34 @@
 from sys import stdin, stdout
-# import numpy as np
+import numpy as np
 N = int(input())
-# A = np.array([], dtype=int)
-A = []
+A = np.array([], dtype=int)
 for row in range(N):
-    rowval = stdin.readline().strip().split(' ')
-    # rowval = np.fromstring(rowval, dtype=int, sep=' ')
-    # A = np.append(A, rowval[row])
-    A.append(int(rowval[row]))
-print(A)
+    rowval = stdin.readline()
+    rowval = np.fromstring(rowval, dtype=int, sep=' ')
+    A = np.append(A, rowval[row])
 
 
 def split_int(listval):
     return list(map(int, listval.split(' ')))
 
 
-Qlen = int(input())
-# prerow = np.array(A)
-prerow = A
-Qrow = stdin.readlines()
-Qrow = list(map(split_int, Qrow))
+def Qfunction(N, A):
+    Qlen = int(input())
+    prerow = np.array(A)
+    Qrow = stdin.readlines()
+    Qrow = list(map(split_int, Qrow))
 
-for line in Qrow:
-    a = line[1]
-    b = line[2]
-    c = line[3]
-    array = [c]*(b-a+1)
-    for i, array_val in enumerate(array):
-        prerow[a-1+i] = prerow[a-1+i] ^ array_val
-    # prerow[a-1:b] = prerow[a-1:b] ^ array
-    # array = np.ones(b-a+1, dtype=int)*c
-    # prerow[a-1:b] = np.bitwise_xor(prerow[a-1:b], array)
-# print(prerow.sum())
-print(sum(prerow))
+    i = 0
+    while(i < Qlen):
+        line = Qrow[0]
+        Qrow.pop(0)
+        a = line[1]
+        b = line[2]
+        c = line[3]
+        array = np.ones(b-a+1, dtype=int)*c
+        prerow[a-1:b] = np.bitwise_xor(prerow[a-1:b], array)
+        i += 1
+    print(prerow.sum())
+
+
+Qfunction(N, A)
