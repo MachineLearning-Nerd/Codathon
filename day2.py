8b30caf27fe8754d2ccf302ae8de79d4cce46f1d
Dinesh <jinjaladinesh@gmail.com>
2020-01-14 23:11:07 +0530

    with generator

--- day2.py
+++ day2.py
@@ -6,31 +6,24 @@ A = np.array([], dtype=int)
 for row in range(N):
     rowval = stdin.readline()
     rowval = np.fromstring(rowval, dtype=int, sep=' ')
-    # rowval = list(map(int, stdin.readline().strip().split(' ')))
-    # A.append(rowval[row])
-    # import pdb
-    # pdb.set_trace()
     A = np.append(A, rowval[row])
 
-Qlen = int(input())
+
+def Qfunction(N, A):
+    Qlen = int(input())
+    for row in range(Qlen):
+        Qrow = stdin.readline()
+        Qrow = np.fromstring(Qrow, dtype=int, sep=' ')
+        a = int(Qrow[1])
+        b = int(Qrow[2])
+        c = int(Qrow[3])
+        array1 = np.ones(b-a+1, dtype=int)*c
+        yield a, b, array1
+
+
 prerow = np.array(A)
-for row in range(Qlen):
-    # Qrow = stdin.readline().split()
-    Qrow = stdin.readline()
-    Qrow = np.fromstring(Qrow, dtype=int, sep=' ')
-    # print(Qrow)
-    a = int(Qrow[1])
-    b = int(Qrow[2])
-    c = int(Qrow[3])
-    array1 = np.ones(b-a+1, dtype=int)*c
-    # array2 = np.zeros(a-1, dtype=int)
-    # array3 = np.zeros(N-b, dtype=int)
-    # newrow = np.append(np.append(array2, array1), array3)
-    # import pdb
-    # pdb.set_trace()
-    # newrow = np.append(np.zeros(a-1, dtype=int),
-    #                    np.ones(b-a+1, dtype=int)*c, np.zeros(N-b, dtype=int))
-    # import pdb
-    # pdb.set_trace()
-    prerow[a-1:b] = prerow[a-1:b] ^ array1
+for a, b, array in Qfunction(N, A):
+    prerow[a-1:b] = np.bitwise_xor(prerow[a-1:b], array)
+
+
 print(prerow.sum())
