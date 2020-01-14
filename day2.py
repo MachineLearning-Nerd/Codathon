be999367b13a1f61bc47db2f1c60c7a11c433acc
Dinesh <jinjaladinesh@gmail.com>
2020-01-15 00:00:00 +0530

    day2 with numpy and timeconsuming

--- day2.py
+++ day2.py
@@ -16,15 +16,19 @@ def Qfunction(N, A):
     Qlen = int(input())
     prerow = np.array(A)
     Qrow = stdin.readlines()
-    Qrow = list(map(split_int, Qrow))
+    # Qrow1 = list(map(split_int, Qrow))
+    # del Qrow
 
     i = 0
-    while(i < Qlen):
-        line = Qrow[0]
-        Qrow.pop(0)
-        a = line[1]
-        b = line[2]
-        c = line[3]
+    # while(i < Qlen):
+    for line in Qrow:
+        # line = Qrow[0]
+        # line = split_int(line)
+        line = line.strip().split(' ')
+        # Qrow.pop(0)
+        a = int(line[1])
+        b = int(line[2])
+        c = int(line[3])
         array = np.ones(b-a+1, dtype=int)*c
         prerow[a-1:b] = np.bitwise_xor(prerow[a-1:b], array)
         i += 1
