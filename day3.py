2ada8894de1853e4d029e6ceac414aa04bed579f
Dinesh <jinjaladinesh@gmail.com>
2020-01-15 02:35:46 +0530

    with thread but only for modulo (pow)

--- day3.py
+++ day3.py
@@ -1,4 +1,5 @@
 from sys import stdin, stdout
+import threading
 import itertools
 
 rowval = list(map(int, stdin.readline().strip().split(' ')))
@@ -6,6 +7,7 @@ price = list(map(int, stdin.readline().strip().split(' ')))
 n = rowval[0]
 m = rowval[1]
 
+i = 0
 data = itertools.product(price, repeat=n)
 
 
@@ -16,12 +18,24 @@ def summation_map(val):
         return 0
 
 
+def checking_function(val):
+    global i
+    if pow(val, 1, m) == 0:
+        i += 1
+
+
 def thread_function(data):
     summation = list(map(sum, data))
-    output = map(summation_map, summation)
-    del summation
-    values = list(output)
-    return sum(values)
+    threads = []
+    for j in range(len(summation)):
+        pop_value = summation.pop(0)
+        # print(pop_value)
+        x = threading.Thread(target=checking_function, args=(pop_value,))
+        threads.append(x)
+        x.start()
+    for thread in threads:
+        thread.join()
 
 
-print(thread_function(data))
+thread_function(data)
+print(i)
