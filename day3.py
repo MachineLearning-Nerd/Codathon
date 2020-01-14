af8e827e55f5e6389d8a2a21639e09bc906b1ec2
Dinesh <jinjaladinesh@gmail.com>
2020-01-15 01:51:57 +0530

    without threading

--- day3.py
+++ day3.py
@@ -1,6 +1,4 @@
-import pdb
 from sys import stdin, stdout
-import threading
 import itertools
 
 rowval = list(map(int, stdin.readline().strip().split(' ')))
@@ -10,26 +8,14 @@ m = rowval[1]
 
 i = 0
 data = itertools.product(price, repeat=n)
-print(data)
 
 
-def thread_function(m):
-    global i, data
+def thread_function(data):
+    global i
     for x in data:
         if sum(x) % m == 0:
             i += 1
 
 
-threads = []
-index = 0
-while(index < len(price)*len(price)):
-    for j in range(10):
-        x = threading.Thread(target=thread_function, args=(m,))
-        threads.append(x)
-        x.start()
-    for thread in threads:
-        thread.join()
-    index += 1
-
-
+thread_function(data)
 print(i)
